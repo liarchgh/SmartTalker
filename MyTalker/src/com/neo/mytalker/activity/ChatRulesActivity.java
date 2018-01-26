ad93dfc68b4f68be252425ffcdc2f259e740040f
NEUShenyang <liarchgh@users.noreply.github.com>
2018-01-26 10:09:20 +0800

    improve rule page

--- MyTalker/src/com/neo/mytalker/activity/ChatRulesActivity.java
+++ MyTalker/src/com/neo/mytalker/activity/ChatRulesActivity.java
@@ -1,5 +1,6 @@
 package com.neo.mytalker.activity;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -7,14 +8,19 @@ import java.util.Map;
 import com.neo.mytalker.R;
 import com.neo.mytalker.adapter.ChatRulesAdapter;
 import com.neo.mytalker.entity.ChatDialogEntity;
+import com.neo.mytalker.myinterface.CharacterParser;
 import com.neo.mytalker.myinterface.CustomDialog;
 import com.neo.mytalker.myinterface.CustomDialog.Builder;
+import com.neo.mytalker.myinterface.PinyinComparator;
+import com.neo.mytalker.myinterface.SideBar;
+import com.neo.mytalker.myinterface.SideBar.OnTouchingLetterChangedListener;
 import com.neo.mytalker.util.ChatRulesManager;
 import com.neo.mytalker.util.SQLiteManager;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,11 +31,12 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ChatRulesActivity extends Activity{
 	ListView mListView;
 	ChatDialogEntity mChatDialogEntity;
-	ArrayList<ChatDialogEntity> mChatDialogEntityList;
+	List<ChatDialogEntity> mChatDialogEntityList;
 	ChatRulesAdapter mChatRulesAdapter;
 	ImageView mAddRulesBtn;
 	CustomDialog.Builder mBuilder;
@@ -38,32 +45,53 @@ public class ChatRulesActivity extends Activity{
 	Context mContext;
 	EditText editTextOne, editTextTwo;
 	String question_temp, answer_temp;
+	TextView mLetterDialog;
+	SideBar mSideBar;
+	CharacterParser mCharacterParser;
+	PinyinComparator mPinyinComparator;
+	List<ChatDialogEntity> mFilterData;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_chat_rules);
+		/*mContext = ChatRulesActivity.this;
+		mSideBar.setContext(mContext);*/
+		mContext = ChatRulesActivity.this;
 		mBuilder = new Builder(this);
 		init();
 		initTestMessage();
+		setListener();
 		setAdapter();
 		setOnClick();
 	}
 	
+	@SuppressLint("DefaultLocale")
 	public void initTestMessage() {
 		List<Map<String, String>> rules = new ChatRulesManager(ChatRulesActivity.this, 0).getAllRules();
 		for(Iterator<Map<String, String>>it = rules.iterator();
 				it.hasNext(); ) {
 			Map<String, String>rule = it.next();
+			char c[] = rule.get(ChatRulesManager.askName).toCharArray();
 			mChatDialogEntity = new ChatDialogEntity(rule.get(ChatRulesManager.askName),
 					rule.get(ChatRulesManager.answerName),
+					String.valueOf(c[0]),
 					Integer.parseInt(rule.get(SQLiteManager.COL_NAME_ID)));
+			String pinyin = mCharacterParser.getSelling(rule.get(ChatRulesManager.askName));
+			String sortString = pinyin.substring(0, 1).toUpperCase();
+			if(sortString.matches("[A-Z]")){
+				mChatDialogEntity.setSortLetters(sortString.toUpperCase());
+			}else{
+				mChatDialogEntity.setSortLetters("#");
+			}
 			mChatDialogEntityList.add(mChatDialogEntity);
 		}
 	}
 	
 	public void init() {
+		mCharacterParser = CharacterParser.getInstance();
+		mPinyinComparator = new PinyinComparator();
 		mChatDialogEntityList = new ArrayList<ChatDialogEntity>();
 		mListView = (ListView) this.findViewById(R.id.rules_list);
 		mChatRulesAdapter = new ChatRulesAdapter(mChatDialogEntityList, this);
@@ -71,9 +99,27 @@ public class ChatRulesActivity extends Activity{
 		mView = LayoutInflater.from(ChatRulesActivity.this).inflate(R.layout.dialog_layout, null, false);
 		editTextOne = (EditText) mView.findViewById(R.id.edit_one);
 		editTextTwo = (EditText) mView.findViewById(R.id.edit_two);
+		mLetterDialog = (TextView) this.findViewById(R.id.letter_dialog);
+		mSideBar = (SideBar) this.findViewById(R.id.sidebar);
+		mSideBar.setTextView(mLetterDialog);
+	}
+	
+	public void setListener() {
+		mSideBar.setOnTouchingLetterChangedListener(new OnTouchingLetterChangedListener() {
+			
+			@Override
+			public void onTouchingLetterChanged(String s) {
+				// TODO Auto-generated method stub
+				int position = mChatRulesAdapter.getPositionForSection(s.charAt(0));
+				if(position != -1) {
+					mListView.setSelection(position);
+				}
+			}
+		});
 	}
 	
 	public void setAdapter() {
+		Collections.sort(mChatDialogEntityList, mPinyinComparator);
 		mListView.setAdapter(mChatRulesAdapter);
 	}
 	
@@ -100,6 +146,14 @@ public class ChatRulesActivity extends Activity{
 						
 						mChatDialogEntityList.get(positionTemp).setQuestion(question_temp);
 						mChatDialogEntityList.get(positionTemp).setAnswer(answer_temp);
+						String pinyin = mCharacterParser.getSelling(question_temp);
+						String sortString = pinyin.substring(0, 1).toUpperCase();
+						if(sortString.matches("[A-Z]")){
+							mChatDialogEntity.setSortLetters(sortString.toUpperCase());
+						}else{
+							mChatDialogEntity.setSortLetters("#");
+						}
+						Collections.sort(mChatDialogEntityList, mPinyinComparator);
 						mChatRulesAdapter.updateData(mChatDialogEntityList);
 						mCustomDialog.dismiss();
 					}
@@ -130,10 +184,18 @@ public class ChatRulesActivity extends Activity{
 						
 						new ChatRulesManager(ChatRulesActivity.this, 0).addRule(question_temp, answer_temp);
 
-						mChatDialogEntity = new ChatDialogEntity(question_temp, answer_temp, size);
+						mChatDialogEntity = new ChatDialogEntity(question_temp, answer_temp, String.valueOf((question_temp.toCharArray())[0]) , size);
 				//		mChatRulesAdapter.addItem(mChatDialogEntity);
 						mChatDialogEntityList.add(mChatDialogEntity);
-						Log.i("click", "SSS"+question_temp);
+				//		Log.i("click", "SSS"+question_temp);
+						String pinyin = mCharacterParser.getSelling(question_temp);
+						String sortString = pinyin.substring(0, 1).toUpperCase();
+						if(sortString.matches("[A-Z]")){
+							mChatDialogEntity.setSortLetters(sortString.toUpperCase());
+						}else{
+							mChatDialogEntity.setSortLetters("#");
+						}
+						Collections.sort(mChatDialogEntityList, mPinyinComparator);
 						mChatRulesAdapter.updateData(mChatDialogEntityList);
 						mCustomDialog.dismiss();
 					}
@@ -148,6 +210,8 @@ public class ChatRulesActivity extends Activity{
 			}
 		});
 	}
+	
+	
 	public void showEditDialog(String alertText,String qText, String aText, String confirmBtn, String cancleBtn, View.OnClickListener conFirmListener, View.OnClickListener cancelListener) {
 		mCustomDialog = mBuilder.setMessage(alertText)
 				.seteditOne(qText)
