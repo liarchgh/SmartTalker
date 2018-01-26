bc65ba44454fef7781bf255f6e7ebd6b3438ff16
NEUShenyang <liarchgh@users.noreply.github.com>
2018-01-26 09:37:05 +0800

    add some file

--- MyTalker/src/com/neo/mytalker/activity/FeatureShowActivity.java
+++ MyTalker/src/com/neo/mytalker/activity/FeatureShowActivity.java
@@ -2,45 +2,51 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.neo.mytalker.R;
 import com.neo.mytalker.adapter.FeaturesPagerAdapter;
+import com.neo.mytalker.fragments.FeatureFinalPageFragment;
 import com.neo.mytalker.fragments.FeaturePageFragment;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
-import android.widget.ImageView;
 
 public class FeatureShowActivity extends FragmentActivity {
-//	Button mQuitAppButton;
+	// Button mQuitAppButton;
 	SharedPreferences mSharedPreferences;
 	int count;
 	Intent myIntent;
 	SharedPreferences.Editor editor;
+	List<Fragment> mVs;
+	ViewPager mVp;
+	FeaturesPagerAdapter mFpa;
+	Timer mTimer;
+	boolean isAuto = true;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_feature);
-		
+
 		mSharedPreferences = getSharedPreferences("AppCountss", MODE_PRIVATE);
 		count = mSharedPreferences.getInt("count", 0);
-		
-		
-		
+
 		/************************************
 		editor = mSharedPreferences.edit();
 		editor.putInt("count", 0);
@@ -49,35 +55,34 @@ public class FeatureShowActivity extends FragmentActivity {
 		Log.i("DHJ", String.valueOf(count));
 		*************************************/
 
-		if(count == 0) {
+		if (count == 0) {
 			setAnimation();
 			editor = mSharedPreferences.edit();
 			editor.putInt("count", 1);
 			editor.commit();
-				
-		}else {
+			// initViewPager();
+		} else {
 			Log.i("DHJ", String.valueOf(count));
 			myIntent = new Intent();
 			myIntent.setClass(FeatureShowActivity.this, ChatActivity.class);
 			startActivity(myIntent);
 			this.finish();
 		}
-	
+
 	}
-	
+
 	public void jump2Home(View v) {
 		Intent it = new Intent();
 		it.setClass(FeatureShowActivity.this, ChatActivity.class);
 		FeatureShowActivity.this.startActivity(it);
 	}
-	
+
 	public void quitApp(View v) {
 		this.finish();
 	}
-	
-	public void setAnimation()
-	{
-		View logo=findViewById(R.id.feature_splash);
+
+	public void setAnimation() {
+		View logo = findViewById(R.id.feature_splash);
 		ObjectAnimator animator1 = ObjectAnimator.ofFloat(logo, "alpha", 1f, 0f);
 		animator1.setDuration(1000);
 		animator1.addListener(new AnimatorListenerAdapter() {
@@ -93,15 +98,74 @@ public class FeatureShowActivity extends FragmentActivity {
 			}
 		});
 		animator1.start();
-		
+
 	}
-	public void initViewPager()
-	{
+
+	public void initViewPager() {
 		Log.i("DHJ", String.valueOf(count));
-		List<FeaturePageFragment> vs = new ArrayList<FeaturePageFragment>();
-		vs.add(new FeaturePageFragment("在这里你可以发送和我的对话内容",R.drawable.feat_1));
-		vs.add(new FeaturePageFragment("点击+号打开功能面板\r\n享受更优质的功能服务",R.drawable.feat_2));
-		ViewPager vp = (ViewPager)FeatureShowActivity.this.findViewById(R.id.featureShowPages);
-		vp.setAdapter(new FeaturesPagerAdapter(getSupportFragmentManager(),vs));
+		mVs = new ArrayList<Fragment>();
+		mVs.add(new FeaturePageFragment("在这里你可以发送和我的对话内容", R.drawable.feat_1));
+		mVs.add(new FeaturePageFragment("点击+号打开功能面板\r\n享受更优质的功能服务", R.drawable.feat_2));
+		mVs.add(new FeatureFinalPageFragment("让我们开始吧", this));
+		mVp = (ViewPager) findViewById(R.id.feature_show_pages);
+		mFpa = new FeaturesPagerAdapter(getSupportFragmentManager(), mVs);
+		mVp.setAdapter(mFpa);
+		mVp.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				isAuto = false;
+				mTimer.cancel();
+				return false;
+			}
+
+		});
+		mVp.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				if (mVp.getCurrentItem() < mVp.getChildCount()) {
+					if (isAuto) {
+
+						((FeaturePageFragment) mVs.get(arg0)).InitAnimation();
+					}
+				} else {
+					((FeatureFinalPageFragment) mVs.get(arg0)).InitAnimation();
+				}
+			}
+		});
+		isAuto = true;
+		mTimer = new Timer();
+		mTimer.schedule(new TimerTask() {
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						if (mVp.getCurrentItem() < mVp.getChildCount()) {
+							mVp.setCurrentItem(mVp.getCurrentItem() + 1);
+						} else {
+							mTimer.cancel();
+							isAuto = false;
+						}
+					}
+				});
+
+			}
+		}, 5000, 5000);
+		((FeaturePageFragment) mVs.get(0)).InitAnimation();
 	}
 }
--- MyTalker/src/com/neo/mytalker/adapter/FeaturesPagerAdapter.java
+++ MyTalker/src/com/neo/mytalker/adapter/FeaturesPagerAdapter.java
@@ -2,17 +2,16 @@ package com.neo.mytalker.adapter;
 
 import java.util.List;
 
-import com.neo.mytalker.fragments.FeaturePageFragment;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.view.View;
+
 
 
 public class FeaturesPagerAdapter extends FragmentPagerAdapter {
-	private List<FeaturePageFragment> mFragments;
-	public FeaturesPagerAdapter(FragmentManager fm,List<FeaturePageFragment> vs){
+	private List<Fragment> mFragments;
+	public FeaturesPagerAdapter(FragmentManager fm,List<Fragment> vs){
 		super(fm);
 		// TODO Auto-generated constructor stub
 		mFragments=vs;
